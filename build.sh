28251ca09ccafcdfc9edd8b0e133f2543f26fd3f
Can Bezmen <can.bezmen@mavi.com>
2017-09-27 10:13:18 +0300

    Kubernetes support added.

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+projectArray=(rabbit-sender rabbit-receiver)
+projectVersion=v1
+
 function note() {
     local GREEN NC
     GREEN='\033[0;32m'
@@ -12,20 +15,46 @@ set -e
 
 if [[ $@ == *"compile"* ]]
 then
-  note "Building rabbit-sender"
-  cd rabbit-sender; gradle clean build -x test; cd -
-  note "Building rabbit-receiver"
-  cd rabbit-receiver; gradle clean build -x test; cd -
+  for project in ${projectArray[*]}; do
+    note "Compiling $project..."
+    cd $project; gradle clean build -x test; docker build -f docker/Dockerfile -t $project:$projectVersion .; cd -
+  done
 fi
 
-if [[ $@ == *"start"* ]]
+if [[ $@ == *"start-d9"* ]]
 then
   note "Starting docker compose"
   docker-compose up --build -d
 fi
 
-if [[ $@ == *"stop"* ]]
+if [[ $@ == *"stop-d9"* ]]
 then
   note "Stopping docker compose"
   docker-compose down
 fi
+
+if [[ $@ == *"start-k8"* ]]
+then
+  eval $(minikube docker-env)
+  note "Starting building yaml files..."
+  note "Starting Rabbitmq Server..."
+  kubectl create -f kubernetes/rabbitmq.yaml; sleep 3
+
+  for project in ${projectArray[*]}; do
+    note "Starting $project..."
+    kubectl create -f kubernetes/$project.yaml; sleep 5
+  done
+fi
+
+
+if [[ $@ == *"stop-k8"* ]]
+then
+  note "Stoping kubernetes..."
+  kubectl delete service rabbitmq
+  kubectl delete deployment rabbitmq
+
+  kubectl delete service ${projectArray[*]}
+  kubectl delete deployment ${projectArray[*]}
+
+fi
+#
